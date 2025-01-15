ae429d93b02d743d12245818ef7c029c07c2c873
Mali Oz <malicsalles@gmail.com>
2025-01-15 17:58:56 -0300

    aprendi a ler gbif

--- inst/extras/GBIF.R
+++ inst/extras/GBIF.R
@@ -1,32 +1,30 @@
-library(readr)
+devtools::load_all()
 
-gbifsp_texto <- readr::read_tsv("~/BIOTA/GBIF/0061630-241126133413365/occurrence.tsv", col_types = paste0(rep("c", 297),collapse=""))
+
+
+gbifsp_texto <- read.gbif("~/BIOTA/GBIF/0061630-241126133413365/occurrence.tsv")
+gbifsp_texto <- filter_fields.gbif(gbifsp_texto)
 gbifsp_texto <- remove_empty_cols(gbifsp_texto)
 str(gbifsp_texto)
-summary(gbifsp_texto)
 dim(gbifsp_texto)
-table(gbifsp_texto$stateProvince)
-gbifsp_gps <- readr::read_csv("~/BIOTA/GBIF/0061636-241126133413365/occurrence.csv", col_types = paste0(rep("c", 297),collapse=""))
-table(gbifsp_gps$stateProvince)
-gbifsp_gps <- remove_empty_cols(gbifsp_gps)
-str(gbifsp_gps)
-dim(gbifsp_gps)
-colns <- names(gbifsp_gps)
-gbifsp_gps[250830,]
+sort(table(gbifsp_texto$stateProvince))
+summary(gbifsp_texto$hasCoordinate == "true") # 281297 têm coordenada
 
+gbifsp_gps <- read.gbif("~/BIOTA/GBIF/0061636-241126133413365/occurrence.tsv")
+gbifsp_gps <- filter_fields.gbif(gbifsp_gps)
+gbifsp_gps <- remove_empty_cols(gbifsp_gps)
+sort(table(gbifsp_gps$stateProvince))
+dim(gbifsp_gps) # 367790 linhas
+
+# merge
 ids <- intersect(sort(gbifsp_gps$gbifID), sort(gbifsp_texto$gbifID))
-length(ids)
-all.equal(subset(gbifsp_texto, gbifID %in% ids), subset(gbifsp_gps, gbifID %in% ids))
-gbif_total <- merge(gbifsp_texto, gbifsp_gps, all.x=T)
+length(ids) # 272991 na interseção
+gbif_total <- merge(gbifsp_texto, gbifsp_gps, all=T)
 dim(gbif_total)
-dim(gbifsp_gps)+dim(gbifsp_texto)
+nrow(gbifsp_gps)+nrow(gbifsp_texto) - length(ids)
 
 # linhas duplicadas
 sum(duplicated(gbif_total$gbifID))
-dups <- gbif_total$gbifID[duplicated(gbif_total$gbifID)]
-dim(dups)
-
-sum(duplicated(gbif_total))
 
 # lugares
 sort(table(gbif_total$stateProvince))
