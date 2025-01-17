2454bda18c60c646070d465cb40443b26b250f4c
Mali Oz <malicsalles@gmail.com>
2025-01-17 10:27:42 -0300

    proceed with testing

--- analyses/plantR.R
+++ analyses/plantR.R
@@ -1,5 +1,5 @@
-splinkkey <- 'eZOGLZyihOoCLlAWs3Tx'
 
+devtools::load_all()
 remotes::install_github("LimaRAF/plantR", ref = "dev")
 
 library("plantR")
@@ -10,13 +10,21 @@ occs_splink <- example_intro
 head(occs_splink)
 
 familia <- "Blechnaceae"
+splinkkey <- 'eZOGLZyihOoCLlAWs3Tx'
 occs_splink <- rspeciesLink(family = familia, key = splinkkey)
 occs_gbif <- rgbif2(species = familia,
     country = "BR",
+    stateProvince = "São Paulo",
     n.records = 450000)
 dim(occs_splink)
+dim(occs_gbif)
+occs <- formatDwc(gbif_data = occs_gbif, splink_data = occs_splink)
+names(occs)
 
-devtools::load_all()
+occs <- formatOcc(occs)
+names(occs)
+occs <- remove_empty_cols(occs)
+
 gbif_raw <- readData("./data/0071227-241126133413365.zip")
 str(gbif_raw)
 
