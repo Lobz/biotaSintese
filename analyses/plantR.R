94370236d60436caf56a5960b99aa7d2e18c312d
Mali Oz <malicsalles@gmail.com>
2025-01-17 11:54:38 -0300

    investigate locality

--- analyses/plantR.R
+++ analyses/plantR.R
@@ -1,6 +1,5 @@
 
 devtools::load_all()
-remotes::install_github("LimaRAF/plantR", ref = "dev")
 
 library("plantR")
 
@@ -12,19 +11,39 @@ head(occs_splink)
 familia <- "Blechnaceae"
 splinkkey <- 'eZOGLZyihOoCLlAWs3Tx'
 occs_splink <- rspeciesLink(family = familia, key = splinkkey)
+str(occs_splink)
 occs_gbif <- rgbif2(species = familia,
     country = "BR",
     stateProvince = "São Paulo",
     n.records = 450000)
 dim(occs_splink)
+table(is.na(occs_splink$locality))
 dim(occs_gbif)
+# Check GBIF docs - what's the diff between locality and verbatimLocality
+table(is.na(occs_gbif$locality), is.na(occs_gbif$verbatimLocality))
+# And how is it possible to have only one of them ???
+subset(occs_gbif, is.na(locality) &! is.na(verbatimLocality))$verbatimLocality
+unique(subset(occs_gbif, !is.na(locality) & is.na(verbatimLocality))$locality)
+t(subset(occs_gbif, !is.na(locality) &! is.na(verbatimLocality))
+    [,c("verbatimLocality","locality")])
+
+head(occs_gbif)
 occs <- formatDwc(gbif_data = occs_gbif, splink_data = occs_splink)
-names(occs)
+table(is.na(occs$locality))
+sort(names(occs))
+table(is.na(occs$locality), is.na(occs$loc))
+samp <- sample(1:nrow(occs), 12)
+occs[samp,"locality"]
 
 occs <- formatOcc(occs)
 names(occs)
-occs <- remove_empty_cols(occs)
+occs <- formatLoc(occs)
+t(occs[samp,c("locality", "locality.new", "stateProvince", "municipality", "loc")])
 
+occs[1:10, 167:187]
+
+
+# read data from file
 gbif_raw <- readData("./data/0071227-241126133413365.zip")
 str(gbif_raw)
 
