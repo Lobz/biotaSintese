70168876b76f56c383d21744fb29bff7afa57aab
Mali Oz <malicsalles@gmail.com>
2025-02-28 17:35:55 -0300

    readingfromfile

--- analyses/test_lists.R
+++ analyses/test_lists.R
@@ -4,19 +4,23 @@ library(readxl)
 # The point of this is to create and compare lists from different techniques
 # For this exercise, we are using data about Estação Ecológica de Avaré
 
+# Read list from Catalogo das Plantas das UCs do Brasil
+cl0 <- read.csv(("../../BIOTA/catalogoUCs/lista_CatUCS_avare.csv"))
+
 # Create list using plantR, GBIF, Reflora, splink
 UC_de_interesse <- "ESTAÇÃO ECOLÓGICA DE AVARÉ"
 uc_name <- prepLoc(UC_de_interesse)
-uc_string <- "est(a[çc?][aã?]o|)?.? ecol([óo?]gica)?.?( de)? avar[ée?]"
+uc_string <- "horto florestal|e(st(a[çc?][aã?]o|)?)?.? e(col([óo?]gica)?)?.?( de)? avar[ée?]"
 
 # GBIF data
-gbif_raw_gps <- readData("../../BIOTA/GBIF/0061636-241126133413365.zip", quote = "", na.strings = c("", "NA"))
-gbif_raw_texto <- readData("../../BIOTA/GBIF/0061630-241126133413365.zip", quote = "", na.strings = c("", "NA"))
-dim(gbif_raw_gps$occurrence)
-dim(gbif_raw_texto$occurrence)
-gbif_raw <- merge(gbif_raw_gps$occurrence, gbif_raw_texto$occurrence, all=T)
-dim(gbif_raw)
-write.csv(gbif_raw, "data/gbif_saopaulo.csv")
+# gbif_raw_gps <- readData("../../BIOTA/GBIF/0061636-241126133413365.zip", quote = "", na.strings = c("", "NA"))
+# gbif_raw_texto <- readData("../../BIOTA/GBIF/0061630-241126133413365.zip", quote = "", na.strings = c("", "NA"))
+# dim(gbif_raw_gps$occurrence)
+# dim(gbif_raw_texto$occurrence)
+# gbif_raw <- merge(gbif_raw_gps$occurrence, gbif_raw_texto$occurrence, all=T)
+# dim(gbif_raw)
+# write.csv(gbif_raw, "data/gbif_saopaulo.csv")
+gbif_raw <- read.csv("data/gbif_saopaulo.csv")
 
 sum(grepl(uc_string, gbif_raw$locality, ignore.case = T, perl = T))
 
@@ -55,21 +59,24 @@ occs <- validateTax(occs) # what the diff between this and formatTax?
 occs <- validateDup(occs) # this removes dups? shouldn't we do this before other checks?
 
 write.csv(occs, "data/occs_sp.csv")
+occs <- read.csv("data/occs_sp.csv")
 
 # Filter occs in the selected CU
 avare <- subset(occs, municipality.new == "avare")
 dim(avare)
-table(avare$locality)
-avare <- subset(occs, grepl(uc_name, locality.new))
-dim(avare)
-avare <- subset(occs, grepl(uc_string, locality.new, ignore.case = TRUE, perl = TRUE))
-dim(avare)
+avare1 <- subset(avare, grepl(uc_string, locality, ignore.case = TRUE, perl = TRUE))
+table(avare1$locality.new)
+dim(avare1)
 
 summ <- summaryData(occs)
 
-list <- checkList(occs)
-str(list)
+cl1 <- checkList(avare1)
+dim(cl1)
+table(cl1$family)
+subset(cl1, family.new == "Fabaceae")
 
+# Didn't like this result. Try to create my own checklist from the data treated with plantR
+table(occs$ba)
 
 # Read list created with CatalogoUCsBr
 
