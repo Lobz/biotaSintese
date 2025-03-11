98206e2e0a8055f667c39e9d87c7878b239184e9
Mali Oz <malicsalles@gmail.com>
2025-03-11 16:34:07 -0300

    use parseReflora, read gbif list

--- analyses/test_lists.R
+++ analyses/test_lists.R
@@ -1,5 +1,6 @@
 library(plantR) # used foi reading and cleaning occurrence data
 library(readxl)
+library(stringr)
 
 # The point of this is to create and compare lists from different techniques
 # For this exercise, we are using data about Estação Ecológica de Avaré
@@ -10,7 +11,7 @@ cl0 <- read.csv(("../../BIOTA/catalogoUCs/lista_CatUCS_avare.csv"))
 # Create list using plantR, GBIF, Reflora, splink
 UC_de_interesse <- "ESTAÇÃO ECOLÓGICA DE AVARÉ"
 uc_name <- prepLoc(UC_de_interesse)
-uc_string <- "horto florestal|e(st(a[çc?][aã?]o|)?)?.? e(col([óo?]gica)?)?.?( de)? avar[ée?]"
+uc_string <- "e(st(a[çc?][aã?]o|)?)?.? e(col([óo?]gica)?)?.?( de)? avar[ée?]"
 
 # GBIF data
 # gbif_raw_gps <- readData("../../BIOTA/GBIF/0061636-241126133413365.zip", quote = "", na.strings = c("", "NA"))
@@ -38,7 +39,8 @@ sum(grepl(uc_string, jabot_raw$locality, ignore.case = T, perl = T))
 # Reflora data
 reflora_raw <- read_excel("../../BIOTA/REFLORA/REFLORA.xlsx")
 names(reflora_raw)
-sum(grepl(uc_string, data$locality, ignore.case = T, perl = T))
+reflora_raw <- parseReflora(reflora_raw)
+sum(grepl(uc_string, reflora_raw$locality, ignore.case = T, perl = T))
 
 # Merge and treat data
 occs <- formatDwc(
@@ -66,19 +68,24 @@ avare <- subset(occs, municipality.new == "avare")
 dim(avare)
 avare1 <- subset(avare, grepl(uc_string, locality, ignore.case = TRUE, perl = TRUE))
 table(avare1$locality.new)
-dim(avare1)
+dim(avare1) # 176 records
 
 summ <- summaryData(occs)
 
 cl1 <- checkList(avare1)
-dim(cl1)
+dim(cl1) # 124 different species??
 table(cl1$family)
 subset(cl1, family.new == "Fabaceae")
 
 # Didn't like this result. Try to create my own checklist from the data treated with plantR
-table(occs$ba)
 
 # Read list created with CatalogoUCsBr
 
 # Read list from GBIF species list tool
-gbif_raw <- readData("./data/gbif_estecolavare_specieslist.zip", quote = "", na.strings = c("", "NA"))
+cl2 <- readData("data/gbif_estecolavare_specieslist.zip", quote = "", na.strings = c("", "NA"))
+cl2 <- read.gbif("plantR_input/data/gbif_estecolavare_specieslist/0002783-250127130748423.csv")
+dim(cl2) # 125 species
+cl2 <- formatTax(cl2)
+intersect(cl2$scientificName, avare1$scientificName)
+setdiff(cl2$scientificName.new, cl1$scientificName.new)
+setdiff(cl1$scientificName.new, cl2$scientificName.new)
