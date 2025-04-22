60c1bf95846ae2643d02b55cdc8bfd2626be76bc
Mali Oz <malicsalles@gmail.com>
2025-04-22 19:46:39 -0300

    create list

--- analyses/test_lists.R
+++ analyses/test_lists.R
@@ -6,7 +6,12 @@ devtools::load_all()
 # For this exercise, we are using data about Estação Ecológica de Avaré
 
 # Read list from Catalogo das Plantas das UCs do Brasil
-cl0 <- read.csv(("../../BIOTA/catalogoUCs/lista_CatUCS_avare.csv"))
+cl0 <- read.csv(("data/raw-data/Dados_Catalogo_UCs_Brasil_EEAVARE.csv"))
+names(cl0)
+cl0$scientificName <- substr(cl0$Táxon, nchar(cl0$Família) + 2, nchar(cl0$Táxon))
+cl0 <- formatTax(cl0)
+dim(cl0)
+str(cl0)
 
 # Create list using plantR, GBIF, Reflora, splink
 UC_de_interesse <- "ESTAÇÃO ECOLÓGICA DE AVARÉ"
@@ -38,6 +43,7 @@ occs <- formatDwc(
     )
 occs <- formatOcc(occs)
 occs <- formatLoc(occs)
+
 # Filter occs in Sao Paulo
 occs <- subset(occs, grepl("sao paulo", stateProvince.new))
 occs <- formatCoord(occs)
@@ -45,7 +51,9 @@ occs <- formatTax(occs)
 occs <- validateLoc(occs)
 occs <- validateCoord(occs) # resourse intensive - optimize?
 occs <- validateTax(occs)
+dim(occs)
 occs <- validateDup(occs) # this removes dups? shouldn't we do this before other checks?
+dim(occs)
 
 # join with reflora and gbif
 reflora <- read.csv("data/derived-data/occs_reflora_post_plantR.csv")
@@ -78,6 +86,9 @@ dim(avare1) # 176 records
 avare3 <- merge(avare1, avare2, all=T)
 dim(avare3)
 
+# drop empty cols
+avare3 <- avare3[,sapply(avare3, function(x) !all(is.na(x)))]
+
 write.csv(avare3, "data/derived-data/occs_avare.csv")
 avare3 <- read.csv("data/derived-data/occs_avare.csv")
 
@@ -86,21 +97,33 @@ avare3 <- formatTax(avare3)
 avare3 <- validateLoc(avare3)
 avare3 <- validateCoord(avare3) # resourse intensive - optimize?
 avare3 <- validateTax(avare3, generalist = T)
-avare3 <- validateDup(avare3, remove=T) # this removes dups? shouldn't we do this before other checks?
+avare3 <- validateDup(avare3) # this removes dups? shouldn't we do this before other checks?
 
+avare3$tax.check <- factor(avare3$tax.check, levels = c("unknown", "low", "medium", "high"), ordered = T)
 table(avare3$scientificName.new, avare3$tax.check)
 
-summ <- summaryData(avare3[c(1:64),])
-
-write.csv(avare3[65:67,], "test_data_error_summaryData.csv")
+summ <- summaryData(avare3[1:4,])
+write.csv(avare3[1:4,], "test_data_error_summaryData.csv")
 
 cl1 <- checkList(avare3)
 dim(cl1) # 129 different species??
 table(cl1$family)
-subset(cl1, family.new == "Fabaceae")
 
 # Didn't like this result. Try to create my own checklist from the data treated with plantR
+avaref <- avare3[, c("family.new", "scientificName.new", "year.new", "yearIdentified.new", "institutionCode", "collectionCode.new", "barcode", "identifiedBy.new", "tax.check", "taxon.rank", "downloadedFrom", "numTombo")]
+species <- subset(avaref, taxon.rank == "species")
 
+table(ouro$scientificName.new)
+
+# Get most recent specimen/identification combo
+splitdf <- by(species, species$scientificName.new, function(x) {
+    x[order(x$tax.check, as.numeric(x$year.new), as.numeric(x$yearIdentified.new), na.last=F, decreasing = T),]
+}, simplify = F)
+species <- do.call(rbind, splitdf)
+table(species$tax.check)
+
+setdiff(cl0$scientificName.new, species$scientificName.new)
+setdiff(subset(species, tax.check >= "high")$scientificName.new, cl0$scientificName.new)
 
 # Read list created with CatalogoUCsBr
 
