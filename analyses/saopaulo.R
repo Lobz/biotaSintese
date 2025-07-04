cfc42e8f5952bd5f567646560a28ec5ccfa7f83f
Mali Oz <malicsalles@gmail.com>
2025-07-04 18:21:14 -0300

    using tryAgain for formating Loc

--- analyses/saopaulo.R
+++ analyses/saopaulo.R
@@ -41,15 +41,7 @@ saopaulo <- subset(saopaulo, is.na(country) | grepl("br", tolower(country), fixe
 
 x <- fixLoc(saopaulo)
 
-###### PAUSE
-save(saopaulo, file="temp.RData")
-load("temp.RData")
-formatLoc
-
-noState <- which(saopaulo$resolution.gazetteer == "country")
-x <- saopaulo[noState,]
-
-unique(x[(grepl("s.?o paulo", x$locality.new)),]$stateProvince.new)
+saopaulo <- formatLoc(saopaulo)
 
 # gonna fucking hand redo formatLoc
 # fixLoc is already done, thank you
@@ -70,10 +62,30 @@ fix_sp <- function(x) {
     gsub("s.?.?o? paulo", "sao paulo", x)
 }
 
+finLoc <- function(x) {
+  # strLoc
+  locs <- strLoc(x)
+  locs$loc.string <- prepLoc(locs$loc.string) # priority string
+  if ("loc.string1" %in% names(locs))
+    locs$loc.string1 <- prepLoc(locs$loc.string1) # alternative string 1
+  if ("loc.string2" %in% names(locs))
+    locs$loc.string2 <- prepLoc(locs$loc.string2) # alternative string 2
+
+  # getLoc
+  locs <- getLoc(x = locs)
+  colunas <- c("loc", "loc.correct", "latitude.gazetteer", "longitude.gazetteer", "resolution.gazetteer")
+  colunas <- colunas[colunas %in% names(locs)]
+  x[,colunas] <- NULL
+  x <- cbind.data.frame(x,
+                         locs[, colunas], stringsAsFactors = FALSE)
+  x[x==""] <- NA
+  x <- x[,names(saopaulo)]
+}
 
 # fix state name
-  table(x$stateProvince.new)
-  x$stateProvince.new[grepl("arquipelago (de ?)sao pedro e sao paulo",x$stateProvince.new)] <- "pernambuco"
+saopaulo <- tryAgain(saopaulo, function(x) x$resolution.gazetteer == "country", function(x) {
+  x$locality.new[grepl("arquipelago (de ?)sao pedro e sao paulo",x$stateProvince.new)] <- "arquipelago de sao pedro e sao paulo"
+  x$stateProvince[grepl("arquipelago (de ?)sao pedro e sao paulo",x$stateProvince.new)] <- "pernambuco"
   x$stateProvince.new<- remove_punct(x$stateProvince.new)
   x$stateProvince.new<- gsub("state","",x$stateProvince.new)
   x$stateProvince.new<- gsub("estado","",x$stateProvince.new)
@@ -87,53 +99,85 @@ fix_sp <- function(x) {
   x$stateProvince.new<- remove_spaces(x$stateProvince.new)
   x$stateProvince.new[grepl("sao paulo",x$stateProvince.new)] <- "sao paulo"
   x$stateProvince.new[grepl("s #227;o paulo",x$stateProvince.new)] <- "sao paulo"
-  x$stateProvince.new[grepl("mogy mirim|campinas|sorocaba|peruibe",x$stateProvince.new)] <- "sao paulo"
+
+  # x$municipality.new <- NA
+
+  x <- finLoc(x)
+
+})
+
+
+saopaulo <- tryAgain(saopaulo, function(x) x$resolution.gazetteer == "country", function(x) {
+  x$municipality.new[grepl("ubatuba",x$stateProvince.new)] <- "ubatuba"
+  x$locality.new[grepl("ubatuba",x$stateProvince.new)] <- "ilha anchieta"
+  x$municipality.new[grepl("mogy mirim|campinas|sorocaba|peruibe|ubatuba",x$stateProvince.new) & is.na(x$municipality.new)] <- x$stateProvince[grepl("mogy mirim|campinas|sorocaba|peruibe|ubatuba",x$stateProvince.new) & is.na(x$municipality.new)]
+  x$stateProvince.new[grepl("mogy mirim|campinas|sorocaba|peruibe|ubatuba|vicosa",x$stateProvince.new)] <- "sao paulo"
   x$stateProvince.new[x$stateProvince.new=="sp"] <- "sao paulo"
   table(x$stateProvince.new)
 
-# find state name in municipality name
-x$municipality.new <- fix_sp(x$municipality.new)
-x$stateProvince.new[grepl("sao paulo", x$municipality.new)] <- "sao paulo"
-stateStr <- "state of sao paulo|sao paulo state|&sao paulo|estado de sao paulo|sao paulo -|estado sao paulo"
-x$municipality.new <- gsub(stateStr,"",x$municipality.new)
-x$municipality.new <- gsub(" -|- "," ",x$municipality.new)
-x$municipality.new<- remove_punct(x$municipality.new)
-x$municipality.new<- remove_spaces(x$municipality.new)
+  x$municipality.new <- fix_sp(x$municipality.new)
+  x$stateProvince.new[grepl("sao paulo", x$municipality.new)] <- "sao paulo"
+  stateStr <- "state of sao paulo|sao paulo state|&sao paulo|estado de sao paulo|sao paulo -|estado sao paulo"
+  x$municipality.new <- gsub(stateStr,"",x$municipality.new)
+  x$municipality.new <- gsub(" -|- "," ",x$municipality.new)
+  x$municipality.new<- remove_punct(x$municipality.new)
+  x$municipality.new<- remove_spaces(x$municipality.new)
 
+  x <- finLoc(x)
+})
 
+# get municipalitys with unique name
+munis <- geobr::read_municipality()
+munis <- subset(munis, !duplicated(name_muni))
+states <- geobr::read_state()
+munis$name_state <- states$name_state[match(munis$code_state, states$code_state)]
+munis$name_state_norm <- tolower(rmLatin(munis$name_state))
 
-  # strLoc
-  locs <- strLoc(x)
-  locs$loc.string <- prepLoc(locs$loc.string) # priority string
-  if ("loc.string1" %in% names(locs))
-    locs$loc.string1 <- prepLoc(locs$loc.string1) # alternative string 1
-  if ("loc.string2" %in% names(locs))
-    locs$loc.string2 <- prepLoc(locs$loc.string2) # alternative string 2
+saopaulo <- tryAgain(saopaulo, function(x) x$resolution.gazetteer == "country" & !is.na(x$municipality), function(x) {
+  # find state name in municipality name
+  x$stateProvince.new <- munis$name_state_norm[match(x$municipality, munis$name_muni)]
 
-  # getLoc
-  locs <- getLoc(x = locs)
-  table(locs$stateProvince.new == x$stateProvince.new, useNA="always")
-  colunas <- c("loc", "loc.correct", "latitude.gazetteer", "longitude.gazetteer", "resolution.gazetteer")
-  colunas <- colunas[colunas %in% names(locs)]
-  x[,colunas] <- NULL
-  x <- cbind.data.frame(x,
-                         locs[, colunas], stringsAsFactors = FALSE)
-  table(x$resolution.gazetteer)
-x[x==""] <- NA
+  x <- finLoc(x)
+})
 
-x <- x[,names(saopaulo)]
+saopaulo <- tryAgain(saopaulo, function(x) x$resolution.gazetteer == "country" & !is.na(x$municipality.new), function(x) {
+  # find state name in municipality name
+  x$municipality.new <- fix_sp(x$municipality.new)
+  x$stateProvince.new[grepl("sao paulo", x$municipality.new)] <- "sao paulo"
+  stateStr <- "state of sao paulo|sao paulo state|&sao paulo|estado de sao paulo|sao paulo -|estado sao paulo"
+  x$municipality.new <- gsub(stateStr,"",x$municipality.new)
+  x$municipality.new <- gsub(" -|- "," ",x$municipality.new)
+  x$municipality.new<- remove_punct(x$municipality.new)
+  x$municipality.new<- remove_spaces(x$municipality.new)
+  x$stateProvince.new <- munis$name_state_norm[match(x$municipality.new, tolower(rmLatin(munis$name_muni)))]
 
-saopaulo[noState,] <- x
+  x <- finLoc(x)
+})
 
+# saopaulo1 <- tryAgain(saopaulo, function(x) {x$resolution.gazetteer == "country" & !is.na(x$locality.new) & !is.na(x$stateProvince.new)}, function(x) {
+#   # try without municipality
+#   my_saved <- x$locality.new
+#   x$locality.new <- NA
+#   x <- finLoc(x)
+#   print(my_saved)
+#   print(length(my_saved))
+#   # x$locality.new <- my_saved
+# })
 # table(saopaulo$country.new, useNA="always")
 # sort(table(saopaulo$stateProvince.new, useNA="always"))
+
+###### PAUSE
+save(saopaulo, file="data/derived-data/temp.RData")
+load("data/derived-data/temp.RData")
 
 locs <- getAdmin(saopaulo$loc.correct)
 names(locs)[1]<-"loc.correct.mun"
 saopaulo[,names(locs)] <- NULL
 saopaulo <- cbind(saopaulo,locs)
 
-saopaulo <- subset(saopaulo, NAME_0 == "Brazil")
+saopaulo <- subset(saopaulo, NAME_0 == "Brazil" | is.na(NAME_0))
+
+noCountry <- subset(saopaulo, is.na(NAME_0))
 
 saopaulo <- subset(saopaulo, NAME_1 == "São Paulo" | is.na(NAME_1))
 # sort(table(saopaulo$stateProvince.new, useNA="always"))
