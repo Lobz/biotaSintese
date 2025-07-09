afda20ea8495973d53c53783e8b9b806af6fffa4
Mali Oz <malicsalles@gmail.com>
2025-07-09 12:42:12 -0300

    isolating authorship and separating taxonomy treatement

--- R/isolateAuthorship.R
+++ R/isolateAuthorship.R
@@ -1,11 +1,30 @@
-
-isolateAuthorship <- function(x, save.original.as = "verbatimScientificName") {
+#' Isolate Authorship
+#'
+#' Extract authorship information from scientific name
+#'
+#' @param x A data frame containing scientificName information
+#' @param save.original.as Name for the column where original scientific name will be saved
+#' @param save.original.authorship.as Name for the column where original scientificNameAuthorship will be saved
+#' @param tax.name Name for the column with scientificName information in x
+#' @param tax.author Name for the column with scientificNameAuthorship information in x
+#'
+#' @return The x data.frame with corrected information in tax.name and tax.author columns, plus new columns save.original.as and save.original.authorship.as. If these columns are already present in x, they will be overwritten.
+#'
+#' @importFrom plantR fixAuthors
+#'
+#' @details This function uses plantR::fixAuthors() to extract authorship information
+isolateAuthorship <- function(x,
+    save.original.as = "verbatimScientificName",
+    tax.name = "scientificName",
+    tax.author = "scientificNameAuthorship",
+    save.original.authorship.as = "verbatimScientificNameAuthorship") {
 
     # Create a column to save original name
-    x[,save.original.as] <- x$scientificName
+    x[,save.original.as] <- x[,tax.name]
+    x[,save.original.authorship.as] <- x[,tax.author]
 
-    # Isolate authorship from taxon names that do not have authorship information
-    species <- as.character(unique(x$scientificName[is.na(x$scientificNameAuthorship)]))
+    # Isolate authorship from taxon names
+    species <- as.character(unique(x[,tax.name]))
     species_split <- fixAuthors(species)
 
     # Select only those that were corrected
@@ -16,9 +35,9 @@ isolateAuthorship <- function(x, save.original.as = "verbatimScientificName") {
     print(paste("Isolated authorship for", sum(fix_these), "records."))
 
     # Merge back
-    m <- match(x$verbatimScientificName[fix_these], species_split$orig.name)
-    x$scientificName[fix_these] <- species_split$tax.name[m]
-    x$scientificName[fix_these] <- species_split$tax.name[m]
+    m <- match(x[fix_these,tax.name], species_split$orig.name)
+    x[fix_these, tax.name] <- species_split$tax.name[m]
+    x[fix_these, tax.author] <- species_split$tax.author[m]
 
     x
 }
--- analyses/multilist.R
+++ analyses/multilist.R
@@ -83,79 +83,8 @@ try({
 
     total <- validateLoc(total)
 
-    # Fix some issues with taxonomy:
-
-    # Some records don't have scientificName for some reason
-    noName <- is.na(total$scientificName)
-    table(noName)
-    # if species is present, use that
-    total$scientificName[noName] <- total$species[noName]
-    noName <- is.na(total$scientificName)
-    table(noName)
-    # else, use genus
-    total$scientificName[noName] <- total$genus[noName]
-    noName <- is.na(total$scientificName)
-    table(noName)
-    # last resort, use family
-    total$scientificName[noName] <- total$family[noName]
-    noName <- is.na(total$scientificName)
-    table(noName)
-
-    # Match scientificName to oficial F&FBR backbone
-    total <- formatTax(total)
-
-    # For records that have authorship inside scientific name, we want to remove that
-    total <- tryAgain(total,
-        condition = function(x) {
-            auth <- gsub("\\(","\\\\(",x$scientificNameAuthorship.new)
-            auth <- gsub("\\)","\\\\)",auth)
-            x$tax.notes == "not found" & pairwiseMap(auth, x$scientificName, grepl)
-            },
-        FUN = function(x) {
-            auth <- gsub("\\(","\\\\(",x$scientificNameAuthorship.new)
-            auth <- gsub("\\)","\\\\)",auth)
-            x$scientificName <- str_squish(pairwiseMap(auth, x$scientificName, function(x,y) sub(x, "", y)))
-            x$scientificName <- sub(", \\d+","",x$scientificName)
-            x <- formatTax(x)
-            x
-        })
-
-    total <- tryAgain(total,
-        condition = function(x) {
-            auth <- gsub("\\(","\\\\(",x$scientificNameAuthorship)
-            auth <- gsub("\\)","\\\\)",auth)
-            x$tax.notes == "not found" & pairwiseMap(auth, x$scientificName, grepl)
-            },
-        FUN = function(x) {
-            auth <- gsub("\\(","\\\\(",x$scientificNameAuthorship)
-            auth <- gsub("\\)","\\\\)",auth)
-            x$scientificName <- str_squish(pairwiseMap(auth, x$scientificName, function(x,y) sub(x, "", y)))
-            x$scientificName <- sub(", \\d+","",x$scientificName)
-            x <- formatTax(x)
-            x
-        })
-
-
-    # we're gonna try again without author (see issue #170 in plantR)
-    total <- tryAgain(total, function(x) x$tax.notes == "not found", formatTax, use.author = F)
-
-    # What's still unmatched? Genus rank
-    total <- tryAgain(total, condition = function(x) x$tax.notes == "not found"& x$taxonRank=="GENUS", FUN = formatTax, tax.name = "genus")
-
-    # What's still unmatched? Vars and subspecies
-    total <- tryAgain(total,
-        condition = function(x) x$tax.notes == "not found" & grepl("\\w+ \\w+ \\w", x$scientificName),
-        FUN = function(x) {
-            saved <- x$scientificName
-            x$scientificName <- sub("(\\w+ \\w+ )", "\\1 var. ", x$scientificName)
-            x <- formatTax(x)
-            x$scientificName <- saved
-            x
-        })
-
-    # What's still unmatched? Try again with less rigor?
-    # total <- tryAgain(total, function(x) x$tax.notes == "not found", formatTax, sug.dist=0.8 )
-
+    # Match taxonomy to taxonomy backbone
+    total <- getTaxonId(total)
 
     # Finished; validate taxonomist
     total <- validateTax(total, generalist = T)
