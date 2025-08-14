f1cdc495b54e5e60975c87a495243b2230a6af6b
Mali Oz <malicsalles@gmail.com>
2025-08-14 17:53:41 -0300

    acessory function

--- R/normalizeTaxonRank.R
+++ R/normalizeTaxonRank.R
@@ -1,3 +1,5 @@
 
 taxonRanks <- c("form", "variety", "subspecies", "species", "genus", "family",
                "order", "class", "phylum", "kingdom")
+
+as.taxon.rank <- function(x) factor(x, levels = taxonRanks, ordered=TRUE)
