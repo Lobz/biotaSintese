ee2f58f49093aae8c3fc9ec96bfa0f2e13e81f03
Mali Oz <malicsalles@gmail.com>
2025-08-27 21:04:24 -0300

    updating (some) results

---
+++ analyses/TODOs.md
@@ -0,0 +1,9 @@
+# TO DOs
+
+## getOccs
+
+- add grepl for NAME_3 -> done
+
+## locationTables
+
+- remove all locations that match grep(uc_string) -> done?? maybe redo later
--- analyses/createNameGazeteer.R
+++ analyses/createNameGazeteer.R
@@ -1,7 +1,7 @@
 devtools::load_all()
 library(plantR) # used for reading and cleaning occurrence data
 library(stringr)
-
+library(sf)
 
 tt <- list.files("results/total-treated", full.names = T)
 dtTreated <- lapply(tt, read.csv, na.strings = c("NA",""), colClasses = "character")
@@ -22,6 +22,7 @@ locTable <- function(x) {
         return(NULL)
     }
     x <- subset(x, confidenceLocality != "High")
+    x$locality <- gsub("Sr\\. $","Sr ",x$locality)
 
     locs <- unlist(stringr::str_split(x$locality,",|[.]|;| - "))
     locs <- sub("\\.$","",locs)
@@ -29,7 +30,8 @@ locTable <- function(x) {
     Local <- c(c(x$locality.new, x$locality, x$locality.scrap)[is.na(x$NAME_3)],x$NAME_3)
     Local <- sub("\\.$","",Local)
     Locality <- c(locs[!locs %in% Local], Local)
-    Locality <- Locality[nchar(Locality) > 2 & tolower(Locality) != "sao paulo"]
+    Locality <- Locality[nchar(Locality) > 2 & tolower(rmLatin(Locality)) != "sao paulo"]
+    Locality <- Locality[!tolower(rmLatin(Locality)) %in% c("sao paulo", "brasil", "brazil", "faz", "floresta ombrofila densa", "mata secundaria")]
     LT <- as.data.frame(table(Locality), stringsAsFactors=F, names = c("Locality", "Freq"))
     if(nrow(LT)==0) {
         return(NULL)
@@ -48,7 +50,21 @@ head(x)
 
 tabs <- lapply(dtTreated, locTable)
 TABS <- dplyr::bind_rows(tabs)
-write.csv(TABS, "results/locationsTable.csv", row.names = F)
+# write.csv(TABS, "results/locationsTable.csv", row.names = F)
+TABS2 <- read.csv("results/locationsTable.csv")
+
+TABS3 <- merge(TABS, TABS2, sort=F)
+write.csv(TABS3, "results/locationsTable.csv", row.names = F)
+
+# We should generate a gazeteer for everyone
+# Point data
+shapes <- st_read("data/raw-data/shp_cnuc_2025_03/cnuc_2025_03.shp")
+shapes$nome_uc <- standardize_uc_name(shapes$nome_uc)
+
+dt <- data.frame(country="BR", stateProvince="SP", municipality=ucs$Municípios.Abrangidos, locality=ucs$Nome.da.UC)
+dt$municipality <- gsub("\\(.*\\)", "", dt$municipality)
+dt <- formatLoc(dt)
+
 
 total <- dplyr::bind_rows(dtTreated)
 nrow(total)
