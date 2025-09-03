57427fd1c127792820375d2cd08f3b564b5ab951
Mali Oz <malicsalles@gmail.com>
2025-09-02 22:46:56 -0300

    Revert "optimizing getTaxonId (wip)"
    This reverts commit cb27d09e672bd2fa41d084cf77001e648c66a461.

--- R/getTaxonId.R
+++ R/getTaxonId.R
@@ -1,16 +1,12 @@
 
 getTaxonId <- function(total) {
-    tax.names= c("scientificName", "scientificNameAuthorship")
-    tax.names.new = c("scientificName.new", "scientificNameAuthorship.new")
-    tax.names.v= c("verbatimScientificName", "scientificNameAuthorship")
-    tax.names.v.new = c("verbatimScientificName.new", "scientificNameAuthorship.new")
     # Fix some issues with taxonomy:
 
     # Some records don't have scientificName for some reason
-    noName1 <- is.na(total$scientificName)
-    table(noName1)
+    noName <- is.na(total$scientificName)
+    table(noName)
     # if species is present, use that
-    total$scientificName[noName1] <- total$species[noName1]
+    total$scientificName[noName] <- total$species[noName]
     noName <- is.na(total$scientificName)
     table(noName)
     # else, use genus
@@ -27,25 +23,25 @@ getTaxonId <- function(total) {
 
     # Match scientificName to oficial F&FBR backbone
     if("tax.notes" %in% names(total)) {
-        total <- tryAgain(total, not_found, prepSpecies, use.authors = F)
+        total <- tryAgain(total, not_found, formatTax, use.authors = F)
     } else {
-        total <- prepSpecies(total, use.authors = F)
+        total <- formatTax(total, use.authors = F)
     }
 
     # we're gonna try again with author (see issue #170 in plantR)
-    total <- tryAgain(total, not_found, prepSpecies)
+    total <- tryAgain(total, not_found, formatTax)
 
     # Try again with verbatim
-    total <- tryAgain(total, not_found, prepSpecies, tax.names = tax.names.v, use.author = F)
+    total <- tryAgain(total, not_found, formatTax, tax.name = "verbatimScientificName", use.author = F)
 
     # And again with author
-    total <- tryAgain(total, not_found, prepSpecies, tax.name = tax.names.v)
+    total <- tryAgain(total, not_found, formatTax, tax.name = "verbatimScientificName")
 
     # Isolate authorship
     total[not_found(total),] <- isolateAuthorship(total[not_found(total),], overwrite.authorship = FALSE)
 
     # we're gonna try again without author (see issue #170 in plantR)
-    total <- tryAgain(total, not_found, prepSpecies, use.author = F)
+    total <- tryAgain(total, not_found, formatTax, use.author = F)
 
     # For records that have authorship inside scientific name, we want to remove that
     total <- tryAgain(total,
@@ -59,7 +55,7 @@ getTaxonId <- function(total) {
             auth <- gsub("\\)","\\\\)",auth)
             x$scientificName <- str_squish(pairwiseMap(auth, x$scientificName, function(x,y) sub(x, "", y)))
             x$scientificName <- sub(", \\d+","",x$scientificName)
-            x <- prepSpecies(x)
+            x <- formatTax(x)
             x
         })
 
@@ -74,16 +70,16 @@ getTaxonId <- function(total) {
             auth <- gsub("\\)","\\\\)",auth)
             x$scientificName <- str_squish(pairwiseMap(auth, x$scientificName, function(x,y) sub(x, "", y)))
             x$scientificName <- sub(", \\d+","",x$scientificName)
-            x <- prepSpecies(x)
+            x <- formatTax(x)
             x
         })
 
 
     # Isolate authorship
-    total <- tryAgain(total, not_found, function(x) {prepSpecies(isolateAuthorship(x))})
+    total <- tryAgain(total, not_found, function(x) {formatTax(isolateAuthorship(x))})
 
     # What's still unmatched? Genus rank
-    total <- tryAgain(total, condition = function(x) not_found(x) & x$taxonRank=="genus", FUN = prepSpecies, tax.name = "genus")
+    total <- tryAgain(total, condition = function(x) not_found(x) & x$taxonRank=="genus", FUN = formatTax, tax.name = "genus")
 
     # What's still unmatched? Vars and subspecies
     total <- tryAgain(total,
@@ -91,7 +87,7 @@ getTaxonId <- function(total) {
         FUN = function(x) {
             saved <- x$scientificName
             x$scientificName <- sub("(\\w+ \\w+ )", "\\1 var. ", x$scientificName)
-            x <- prepSpecies(x)
+            x <- formatTax(x)
             x$scientificName <- saved
             x
         })
@@ -100,7 +96,7 @@ getTaxonId <- function(total) {
         FUN = function(x) {
             saved <- x$scientificName
             x$scientificName <- sub("(\\w+ \\w+ )", "\\1 subsp. ", x$scientificName)
-            x <- prepSpecies(x)
+            x <- formatTax(x)
             x$scientificName <- saved
             x
         })
@@ -109,23 +105,25 @@ getTaxonId <- function(total) {
         FUN = function(x) {
             saved <- x$scientificName
             x$scientificName <- sub("(\\w+ \\w+ )", "\\1 f. ", x$scientificName)
-            x <- prepSpecies(x)
+            x <- formatTax(x)
             x$scientificName <- saved
             x
         })
 
     # What's still unmatched? Try again with less rigor?
-    # total <- tryAgain(total, function(x) x$tax.notes == "not found", prepSpecies, sug.dist=0.8 )
+    # total <- tryAgain(total, function(x) x$tax.notes == "not found", formatTax, sug.dist=0.8 )
 
     # Finally, if something is still unmatched, give up and match higher taxon rank
     total <- tryAgain(total,
         condition = function(x) {not_found(x) & grepl("\\w+ \\w+ \\w", x$scientificName)},
         FUN = function(x) {
-            x$scientificName.new <- sub("(^\\w+ \\w+).*", "\\1", x$scientificName)
-            x <- prepSpecies(x, )
+            saved <- x$scientificName
+            x$scientificName <- sub("(^\\w+ \\w+).*", "\\1", x$scientificName)
+            x <- formatTax(x)
+            x$scientificName <- saved
             x
         })
 
 
-    total formatTax()
+    total
 }
