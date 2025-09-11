b5cb3d7bed927919600bcee2985102ab9ab98848
Mali Oz <malicsalles@gmail.com>
2025-09-11 16:19:56 -0300

    refine uc string

--- R/generate_uc_string.R
+++ R/generate_uc_string.R
@@ -2,9 +2,11 @@
 #'
 #' @importFrom plantR rmLatin
 generate_uc_string <- function(x) {
-    s <- tolower(x)
+    # String de regex para de/dos/da/das/,/-
     str_de <- "(,? ?( d[oae]s?)? ?| - )"
-    # intercheangable names
+
+    s <- tolower(x)
+    # interchangeable names
     m <- tolower(paste0(uc_abbrevs$short,"|",uc_abbrevs$long))
     for(n in m)
         s <- sub(n,paste0("(",n,")",str_de),s,perl=T)
@@ -18,11 +20,13 @@ generate_uc_string <- function(x) {
     s <- gsub("[óôõò`]","[óôõòo _?]?",s)
     s <- gsub("[úûüù`]","[úûüùu _?]?",s)
     s <- gsub("[ç`]","[çc _?]?",s)
+    s <- gsub("-","[ -]",s)
     paste0("(",s,")")
 }
 
 uc_abbrevs <- data.frame(
-    short= c("APA", "RPPN", "ARIE", "RDS", "MNE", "FLONA", "PE", "PARNA", "PNM", "EEC"),
+    short= c("APA", "RPPN", "ARIE", "RDS", "MNE", "FLONA", "PE", "PARNA", "PNM",
+             "EEC","ESEC","ESEX","RESEX","REBIO","REVIS","RVS","MONA"),
     long = c("ÁREA DE PROTEÇÃO AMBIENTAL",
             "RESERVA PARTICULAR DO PATRIMÔNIO NATURAL",
             "ÁREA DE RELEVANTE INTERESSE ECOLÓGICO",
@@ -32,23 +36,36 @@ uc_abbrevs <- data.frame(
             "PARQUE ESTADUAL",
             "PARQUE NACIONAL",
             "PARQUE NATURAL MUNICIPAL",
-            "ESTAÇÃO ECOLÓGICA"))
+            "ESTAÇÃO ECOLÓGICA",
+            "ESTAÇÃO ECOLÓGICA",
+            "ESTAÇÃO EXPERIMENTAL",
+            "RESERVA EXTRATIVISTA",
+            "RESERVA BIOLÓGICA",
+            "REFÚGIO DE VIDA SILVESTRE",
+            "REFÚGIO DE VIDA SILVESTRE",
+            "MONUMENTO NATURAL"
+            ))
 
 #' Standardize UC Name
 #'
 #' Convert conservation unit name to long form and correct mistakes
 #'
 #' @param x UC names
+#'
+#' @details In addition to correcting common mistakes and coverting abbreviations to long form, this function also removes connectors between
 standardize_uc_name <- function(x) {
     x <- plantR:::squish(x)
     x <- sub("^AREA", "ÁREA", x)
+    x <- sub(" AREA", "ÁREA", x)
     x <- sub("PATRIM.NIO", "PATRIMÔNIO", x)
+    x <- sub("REFUGIO", "REFÚGIO", x)
     x <- sub("PATRIMÔNIO NATURA ", "PATRIMÔNIO NATURAL ", x, fixed = T)
     x <- sub(" AGUAS", " ÁGUAS", x, fixed = T)
     x <- sub("SITIO", "SÍTIO", x, fixed = T)
     x <- gsub("\"", "", x, fixed = T)
     for(i in 1:nrow(uc_abbrevs)){
-        x <- sub(paste0("^",uc_abbrevs$short[i]), uc_abbrevs$long[i], x)
+        x <- sub(paste0("^", uc_abbrevs$short[i]), uc_abbrevs$long[i], x)
+        # x <- sub(paste0(uc_abbrevs$long[i]," (D[OAE]S?|-) "), paste0(uc_abbrevs$long[i]," "), x)
     }
     x
 }
