ee2785069e5b15969e13b27d537eebcc29019649
Mali Oz <malicsalles@gmail.com>
2025-12-21 22:09:54 -0300

    removed download  code

--- analyses/formatData/splink.R
+++ analyses/formatData/splink.R
@@ -1,100 +1,32 @@
-library(plantR)
 devtools::load_all()
-
-# this will get all results from são paulo
-url_col <- 'https://specieslink.net/ws/1.0/participants?apikey=qUe5HQpZDZH3yFNKnjMj'
-
-data_col <- jsonlite::fromJSON(url_col)
-
-flatten_list <- function(x, parent = "") {
-    codes <- names(x)
-    types <- sapply(x, function(x) x$type)
-    names <- sapply(x, function(x) x$name)
-    ids <- sapply(x, function(x) x$id)
-
-    df <- data.frame(code = codes, name = names, type = types, id = ids, parent = parent)
-
-    dfs <- lapply(x, function(x) {
-        if(!is.null(x$nodes))
-            flatten_list(x$nodes, parent = x$id)
-    })
-
-    dfs <- dplyr::bind_rows(dfs)
-    rbind(df, dfs)
-}
-
-df <- flatten_list(data_col)
-df_col <- subset(df, type=="col")
-
-expected <- read.csv("data/raw-data/spl_sp_resumo_collections.csv")
-expected$code <- stringr::str_squish(expected$code)
-
-expected <- merge(expected, df_col)
-
-sofar <- as.data.frame(table(splsaopaulo$collectionid))
-names(sofar) <- c("id", "num_records")
-
-sofar <- merge(expected, sofar)
-summary(sofar$records-sofar$num_records)
-sofar$id[(sofar$records==sofar$num_records)]
-
-togo <- subset(sofar, records > num_records)
-ids <- togo$id
-
-splsaopaulo <- subset(splsaopaulo, !collectionid %in% ids)
-
-head(togo)
-
-splinkkey <- 'qUe5HQpZDZH3yFNKnjMj'
-for(i in 2:nrow(togo)) {
-    print(paste("Collection:", togo$code[i]))
-    print(paste("Expected records:", togo$records[i]))
-    id <- togo$id[i]
-    one_batch <- rspeciesLink(
-        Scope = "p", # this should filter out animals, but unreliable in filtering out fungi, bacteria, etc
-        stateProvince = "Sao Paulo",
-        collectionID = id,
-        kingdom = "Plantae",
-        key = splinkkey,
-        MaxRecords = 5000)
-    my_list <- list(one_batch)
-    nrecords = 5000
-
-    while(nrow(one_batch) == 5000) {
-        one_batch <- rspeciesLink(
-            Scope = "p", # this should filter out animals, but unreliable in filtering out fungi, bacteria, etc
-            stateProvince = "Sao Paulo",
-            collectionID = id,
-            key = splinkkey,
-            kingdom = "Plantae",
-            MaxRecords = 5000,
-            offset = as.integer(nrecords))
-        my_list[[length(my_list) + 1]] <- one_batch
-        nrecords <- nrecords+5000
-        print(nrecords)
+# splink data
+splink_files <- list.files("data-input/splink", pattern = "*.txt$", full.names = TRUE)
+print("Reading splink files:")
+print(splink_files)
+splink_data_raw <- lapply(splink_files, read.csv, sep="\t", na.strings = c("", "NA"), quote = "")
+splink <- splink_data_raw[[1]]
+if(length(splink_data_raw) > 1) {
+    print("Merging splink databases...")
+    for(i in 2:length(splink_data_raw)){
+        splink <- merge(splink, splink_data_raw[[i]], all=T)
     }
-
-    bound <- dplyr::bind_rows(my_list)
-    print(nrow(bound))
-    splsaopaulo <- dplyr::bind_rows(splsaopaulo, bound)
-    print(nrow(splsaopaulo))
-    splsaopaulo <- subset(splsaopaulo, kingdom == "Plantae")
-    splsaopaulo$downloadedFrom <- "SPLINK"
-
-    print(nrow(splsaopaulo))
-    save(splsaopaulo, file="data/raw-data/spl_saopaulo.RData")
 }
+
+print(paste("Found",nrow(splink), "observations."))
 
 # Normalize taxonRank and basisOfRecord
-table(splsaopaulo$basisofrecord, useNA="always")
-splsaopaulo$verbatimbasisofrecord <- splsaopaulo$basisofrecord
-splsaopaulo$basisofrecord <- sub("([a-z])([A-Z])", "\\1_\\2", splsaopaulo$basisofrecord)
-splsaopaulo$basisofrecord[which(startsWith(splsaopaulo$basisofrecord, "Machine"))] <- "MACHINE_OBSERVATION"
-splsaopaulo$basisofrecord[which(startsWith(splsaopaulo$basisofrecord, "Preserved"))] <- "PRESERVED_SPECIMEN"
-splsaopaulo$basisofrecord[which(startsWith(splsaopaulo$basisofrecord, "Xil"))] <- "PRESERVED_SPECIMEN"
-splsaopaulo$basisofrecord[splsaopaulo$basisofrecord=="Carpo"] <- "PRESERVED_SPECIMEN"
-splsaopaulo$basisofrecord <- toupper(splsaopaulo$basisofrecord)
-splsaopaulo$basisofrecord <- as.basisOfRecord(splsaopaulo$basisofrecord)
+table(splink$basisofrecord, useNA="always")
+splink$verbatimbasisofrecord <- splink$basisofrecord
+splink$basisofrecord <- sub("([a-z])([A-Z])", "\\1_\\2", splink$basisofrecord)
+splink$basisofrecord[which(startsWith(splink$basisofrecord, "Machine"))] <- "MACHINE_OBSERVATION"
+splink$basisofrecord[which(startsWith(splink$basisofrecord, "Preserved"))] <- "PRESERVED_SPECIMEN"
+splink$basisofrecord[which(startsWith(splink$basisofrecord, "Xil"))] <- "PRESERVED_SPECIMEN"
+splink$basisofrecord[splink$basisofrecord=="Carpo"] <- "PRESERVED_SPECIMEN"
+splink$basisofrecord <- toupper(splink$basisofrecord)
+splink$basisofrecord <- as.basisOfRecord(splink$basisofrecord)
 
-save(splsaopaulo, file="data/raw-data/spl_saopaulo.RData")
+splink$recordedBy <- splink$collector
+splink$recordNumber <- splink$fieldnumber
+splink$scientificNameAuthorship <- splink$scientificnameauthor
+save(splink, file="data-tmp/splink.RData")
 # todo: decide what to do with barcode NA
