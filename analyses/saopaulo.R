3a35913fba283dc707df55754337e5f6489d3bfc
Mali Oz <malicsalles@gmail.com>
2026-03-19 18:40:21 -0300

    separate deduplicate

--- analyses/saopaulo.R
+++ analyses/saopaulo.R
@@ -4,6 +4,7 @@ library(plantR)
 
 print("Loading data...")
 load("data-tmp/reflora_gbif_jabot_splink_saopaulo.RData")
+print(paste("Found", nrow(saopaulo), "records."))
 
 # get municipalities with unique name
 print("Loading municipality gazetteer...")
@@ -231,7 +232,7 @@ saopaulo <- getTaxonId(saopaulo)
 # loading the WFO and WCVP backbones into a temporary environment
 data(list = c("wfoNames", "wcvpNames"), package = "plantRdata")
 # using the World Flora Online
-saopaulo <- tryAgain(saopaulo, not_found, getTaxonId, db = wfoNames)
+# saopaulo <- tryAgain(saopaulo, not_found, getTaxonId, db = wfoNames)
 # using the World Checklist of Vascular Plants
 saopaulo <- tryAgain(saopaulo, not_found, getTaxonId, db = wcvpNames)
 
@@ -272,29 +273,3 @@ saopaulo$recordID <- 1:nrow(saopaulo) # I need a unique ID for this
 
 print("Saving...")
 save(saopaulo,file="data-tmp/reflora_gbif_jabot_splink_saopaulo.RData")
-
-print("Removing duplicates...")
-loc.names <- c(loc.cols, paste0(loc.cols, ".new"), "longitude.gazetteer", "latitude.gazetteer")
-names(loc.names) <- loc.names
-loc.names <- c(loc.str = "loc.correct", res.gazet = "resolution.gazetteer", res.orig =
-    "resol.orig", loc.check = "loc.check", loc.names)
-my_valDup <- function(x) validateDup(x, noNumb = NA, noYear = NA, noName = NA, prop=0.25,
-  comb.fields = list(
-    c("family", "col.last.name", "col.number", "col.loc"),
-    c("family", "col.last.name", "col.number", "col.year"),
-    # c("family", "col.year", "col.number", "col.loc"),
-    c("species", "col.last.name", "col.number", "col.year"),
-    c("species", "col.last.name", "col.number", "col.loc"),
-    c("col.year", "col.last.name", "col.number", "col.loc")),
-  tax.names = c(family = "family.new", species = "scientificName.new", tax.auth =
-    "scientificNameAuthorship.new", det.name = "identifiedBy.new", det.year =
-    "yearIdentified.new", tax.check = "tax.check", tax.rank = "taxon.rank", status =
-    "scientificNameStatus", id = "id", name.full = "scientificNameFull", gen = "genus.new", sp = "species.new"),
-  geo.names = c(lat = "decimalLatitude.new", lon = "decimalLongitude.new", org.coord =
-    "origin.coord", prec.coord = "precision.coord", geo.check = "geo.check", datum = "geodeticDatum"),
-  loc.names = loc.names, ignore.miss = T)
-
-sp_deduped <- my_valDup(saopaulo)
-
-print("Saving...")
-save(sp_deduped,file="data-tmp/reflora_gbif_jabot_splink_saopaulo_deduped.RData")
